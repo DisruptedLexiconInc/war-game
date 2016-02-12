2733269e14ae3120ab87976f3638fc3d0f1ccbe7
Raymond King <raymond@hivetechnologies.net>
2016-02-12 05:46:50 -0600

    angular-js-client Updated misions controller and page to call backend server to receive a list of missions and display to user Changed battle controller list to not have bullets

--- war-game-parent/war-game-angularjs/app.js
+++ war-game-parent/war-game-angularjs/app.js
@@ -3,87 +3,90 @@ var myApp = angular.module('myApp', ['ngRoute', 'ngResource']);
 
 //ROUTE CONFIG
 myApp.config(function ($routeProvider) {
-   
+
     $routeProvider
-    
+
         .when('#/', {
-            templateUrl: '/',
-            controller: 'mainController'
-        })
-    
-        .when('/missions', {
-            templateUrl: 'pages/missions.html',
-            controller: 'missionsController'
-        })
+        templateUrl: '/',
+        controller: 'mainController'
+    })
 
-        .when('/battle', {
-            templateUrl: 'pages/battle.html',
-            controller: 'battleController'
-        })
+    .when('/missions', {
+        templateUrl: 'pages/missions.html',
+        controller: 'missionsController'
+    })
 
-        .when('/login', {
-            templateUrl: 'pages/login.html',
-            controller: 'loginController'
-        })
-    
-        .when('/logout', {
-            templateUrl: 'pages/logout.html',
-            controller: 'logoutController'
-        })
+    .when('/battle', {
+        templateUrl: 'pages/battle.html',
+        controller: 'battleController'
+    })
 
-        .when('/allies', {
-            templateUrl: 'pages/allies.html',
-            controller: 'alliesController'
-        })
+    .when('/login', {
+        templateUrl: 'pages/login.html',
+        controller: 'loginController'
+    })
 
-        .when('/welcome', {
-            templateUrl: 'pages/welcome.html',
-            controller: 'welcomeController'
-        });
-}); 
+    .when('/logout', {
+        templateUrl: 'pages/logout.html',
+        controller: 'logoutController'
+    })
+
+    .when('/allies', {
+        templateUrl: 'pages/allies.html',
+        controller: 'alliesController'
+    })
+
+    .when('/welcome', {
+        templateUrl: 'pages/welcome.html',
+        controller: 'welcomeController'
+    });
+});
 
 // CONTROLLERS
 myApp.controller('mainController', ['$scope', '$log', 'loginService', function ($scope, $log, loginService) {
-    
+
 }]);
 
-myApp.controller('welcomeController', ['$scope', '$log', 'loginService', function ($scope, $log, loginService) {                                           
+myApp.controller('welcomeController', ['$scope', '$log', 'loginService', function ($scope, $log, loginService) {
     $log.info('Welcome ' + loginService.username);
-    
+
     $scope.name = function () {
         return loginService.username;
     };
 }]);
 
 // CONTROLLERS
-myApp.controller('loginController', ['$scope', '$log', '$location', 'loginService', '$http', function ($scope, $log, $location, loginService, $http) {    
+myApp.controller('loginController', ['$scope', '$log', '$location', 'loginService', '$http', function ($scope, $log, $location, loginService, $http) {
     $log.info('User loaded login screen');
-    
-    if(loginService.id > 0) {
+
+    if (loginService.id > 0) {
         $log.debug('User already loged in. Redirecting to home page: ' + loginService.username);
-         $location.url('/welcome');        
+        $location.url('/welcome');
     }
-    
+
     $scope.loginUser = function () {
         var username = document.getElementById('login-username').value;
         var password = document.getElementById('login-password').value;
-        
+
         $log.debug('login function called by ' + username + ' with password: ' + password);
-        
-        var data = {username : username, password : password};
-        
+
+        var data = {
+            username: username,
+            password: password
+        };
+
         $http.post('http://127.0.0.1:51849/login', JSON.stringify(data)).success(function (data, status) {
             $log.debug('JSON login rsponse is' + JSON.stringify(data));
             loginService.update(data);
-            
+
             $log.debug(loginService);
-            if(loginService.id > 0) {
+            if (loginService.id > 0) {
                 $log.debug('user successfully logged in. redirecting to welcome screen.');
                 $location.url('/welcome');
-            }else {
+            } else {
                 $log.warn('users login failed.');
                 alert('Login failed');
-            } 
+            }
         }).error(function (status) {
             $log.warn('error happened: ' + status);
             loginService.username = "";
@@ -91,27 +94,31 @@ myApp.controller('loginController', ['$scope', '$log', '$location', 'loginServic
     };
 }]);
 
-myApp.controller('battleController', ['$scope', '$log', '$http', 'loginService', function($scope, $log, $http, loginService) {                                            
-   $log.info('User loaded battle screen ' + loginService.username);
-    
+myApp.controller('battleController', ['$scope', '$log', '$http', 'loginService', function ($scope, $log, $http, loginService) {
+    $log.info('User loaded battle screen ' + loginService.username);
+
     function Usercontext(id, username, password) {
-            this.id = id;
-            this.username = username;
-            this.password = password;
+        this.id = id;
+        this.username = username;
+        this.password = password;
     }
-    
+
     $scope.items = [];
-    $scope.add = function(data) {
+    $scope.add = function (data) {
         $scope.items.push(data);
     }
-    
-    var sendData = {id: loginService.id, username : loginService.username, password : loginService.password};
-    
+
+    var sendData = {
+        id: loginService.id,
+        username: loginService.username,
+        password: loginService.password
+    };
+
     $http.post('http://127.0.0.1:51849/battles/opponent', sendData).success(function (data, status) {
         $log.debug('***opponent response');
         $log.debug(data);
-        
-        angular.forEach(data, function(singData, index) {
+
+        angular.forEach(data, function (singData, index) {
             $log.debug(index + ' ' + singData);
             $scope.add(singData);
         })
@@ -120,37 +127,59 @@ myApp.controller('battleController', ['$scope', '$log', '$http', 'loginService',
     });
 }]);
 
-myApp.controller('alliesController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {                                            
-      $log.info('User loaded allies screen');
+myApp.controller('missionsController', ['$scope', '$log', '$routeParams', 'loginService', '$location', '$http', function ($scope, $log, $routeParams, loginService, $location, $http) {
+    $log.info('User loaded missions screen');
 
+    if (loginService.id < 1) {
+        $log.debug("user not logged in. redirecting to login page.");
+
+        $location.url('/login');
+    }
+
+    $scope.missions = [];
+    $scope.addMission = function (data) {
+        $scope.missions.push(data);
+    }
+
+    $http.get('http://127.0.0.1:51849/missions/').success(function (data, status) {
+        $log.debug('JSON missions rsponse is' + JSON.stringify(data));
+
+        angular.forEach(data, function (singData, index) {
+            $log.debug(index + ' ' + singData);
+            $scope.addMission(singData);
+        });
+
+    }).error(function (status) {
+        $log.warn('error happened: ' + status);
+    });
 }]);
 
-myApp.controller('missionsController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {                                            
-   $log.info('User loaded missions screen');
+myApp.controller('alliesController', ['$scope', '$log', '$routeParams', 'loginService', function ($scope, $log, $routeParams, loginService) {
+    $log.info('User loaded allies screen');
 }]);
 
-myApp.controller('logoutController', ['$scope', '$log', 'loginService', '$location', function($scope, $log, loginService, $location) {                                            
-   $log.info('User loaded logout screen ' + loginService.username);
-    
+myApp.controller('logoutController', ['$scope', '$log', 'loginService', '$location', function ($scope, $log, loginService, $location) {
+    $log.info('User loaded logout screen ' + loginService.username);
+
     loginService.logout();
-    
+
     $location.url('/login');
 }]);
 
 //SERVICES
-myApp.service('loginService', function() {
-    
+myApp.service('loginService', function () {
+
     var self = this;
     this.id = '';
     this.username = '';
     this.password = ''
-    
-    this.update = function(data) {
+
+    this.update = function (data) {
         this.username = data.username;
         this.id = data.id;
     }
-    
-    this.logout = function() {
+
+    this.logout = function () {
         this.username = "";
         this.id = "";
     }
